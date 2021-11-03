264b9d7ce832531ac6c85e5a38957746d33967fe
nathanvictorparks <nathan.parks123@gmail.com>
2021-11-03 13:13:10 -0600

    Added .output() to linked_list

--- linked_list/linked_list.cpp
+++ linked_list/linked_list.cpp
@@ -1,21 +1,19 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
 template<typename T>
 class Node {
  private:
- 	int size;
 	Node* tail;
 	T data;
  public:
 	Node() {
-		size = 0;
 		tail = nullptr;
 	}
 	Node(T in) {
-		size=0;
 		data = in;
 		tail = nullptr;
 	}
@@ -37,18 +35,38 @@ class Node {
 	}
 	void append(T in) {
 		if (this->tail) {
-			(*tail).append(in);
+			tail->append(in);
 		}
 		else {
 			Node<T>* tmp = new Node(in);
 			this->tail = tmp;
 		}
-	}
+	}	
+	vector<T> output() {
+		static vector<T> tmp;
+		vector<T> result;
+		if(this->tail) {
+			tmp.push_back(this->data);
+			return this->tail->output();
+		}
+		else {
+			tmp.push_back(this->data);
+			result.resize(tmp.size());
+			for (unsigned int i=0; i<tmp.size(); i++) {
+				result[i] = tmp[i];
+			}
+			tmp.resize(0);
+			return result;
+		}		
+	}	
 };
 
 
 int main() {
 	Node<int> root(1);
 	root.append(5);
-
+	vector<int> s;
+	s = root.output();
+	for (unsigned int i=0; i<s.size(); i++)
+		cout << s[i] << endl;
 }
